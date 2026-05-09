2b8dff7950d9a5864247fca8ac70c194db93ff79
Rakesh Ranjan Jena <rakeshranjanjena7@gmail.com>
2026-05-09 16:35:04 +0530

    Career & Provincial frontend page added

---
+++ resources/views/frontend/provincial/index.blade.php
@@ -0,0 +1,21 @@
+@extends('web')
+@section('content')
+    <style>
+        .shock-header .navbar .navbar-nav .nav-link {
+            color: black !important;
+        }
+    </style>
+    <!-- Intro -->
+    <section class="shock-section has-holder pt-4 pb-8">
+        <div class="container max-w-85">
+            <div class="basic-intro text-center mb-5">
+                <h1 class="title black">
+                    Provincial
+                </h1>
+                <p class="sub-title black">
+                    Provincial is a collection of 10,000 unique NFTs living on the Ethereum blockchain. Each Provincial NFT is a unique piece of digital art that represents a different province in the world. The collection is designed to celebrate the diversity and beauty of the world's provinces, and to provide a platform for artists and collectors to connect and share their love of art and culture.
+                </p>
+            </div>
+        </div>
+    </section>
+@endsection
